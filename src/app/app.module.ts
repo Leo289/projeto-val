028a9d70d6dbee6cdd9261bf1c295efc31f62b70
Leo2891~ <als1975@protonmail.com>
2023-05-18 13:14:18 -0300

    Mensagem do commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+// app.module.ts
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
@@ -6,34 +7,44 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+import { environment } from '../environments/environment';
+
+
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { AngularFireFunctionsModule } from '@angular/fire/compat/functions';
+import { AngularFireMessagingModule } from '@angular/fire/compat/messaging';
+import { AngularFirePerformanceModule } from '@angular/fire/compat/performance';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
 
 import { provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAnalytics,getAnalytics,ScreenTrackingService,UserTrackingService } from '@angular/fire/analytics';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
+
+import { provideAnalytics, getAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
 import { provideFirestore } from '@angular/fire/firestore';
-import { provideFunctions,getFunctions } from '@angular/fire/functions';
-import { provideMessaging,getMessaging } from '@angular/fire/messaging';
-import { providePerformance,getPerformance } from '@angular/fire/performance';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideFunctions, getFunctions } from '@angular/fire/functions';
+import { provideMessaging, getMessaging } from '@angular/fire/messaging';
+import { providePerformance, getPerformance } from '@angular/fire/performance';
+import { provideStorage, getStorage } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire/compat';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+
 
 import { FormularioComponent } from './componentes/formulario/formulario.component';
 import { FormsModule } from '@angular/forms';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HeaderComponent } from './componentes/header/header.component';
+import { LinkServicosComponent } from './componentes/link-servicos/link-servicos.component';
 import { CorpoComponent } from './componentes/corpo/corpo.component';
+
 import { GaleriadefotosComponent } from './componentes/galeriadefotos/galeriadefotos.component';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { LoginComponent } from './componentes/login/login.component';
 import { AgendamentoComponent } from './componentes/agendamento/agendamento.component';
-
-
-
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 
 
 @NgModule({
@@ -46,9 +57,7 @@ import { AgendamentoComponent } from './componentes/agendamento/agendamento.comp
     GaleriadefotosComponent,
     LoginComponent,
     AgendamentoComponent,
-    
-
-
+    LinkServicosComponent
   ],
   imports: [
     BrowserModule,
@@ -69,12 +78,10 @@ import { AgendamentoComponent } from './componentes/agendamento/agendamento.comp
     AngularFireModule.initializeApp(environment.firebase),
     MatSnackBarModule,
     FontAwesomeModule
-
-
   ],
-
   providers: [
-    ScreenTrackingService,UserTrackingService
+    ScreenTrackingService,
+    UserTrackingService,
   ],
   bootstrap: [AppComponent]
 })
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+// main.ts
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+
+/* import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'; */
 
 if (environment.production) {
   enableProdMode();
@@ -10,3 +14,6 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+/*const firebaseApp = initializeApp(environment.firebase);
+const db = getFirestore(firebaseApp); */
